5485c080bfefbeb9f19f3c1c108c45c9877d10bb
HarshaKundlikar <hkundlikar@gmail.com>
2024-11-29 16:47:14 +0530

    One ApI test case added

---
+++ src/test/java/com/Testingacadmy/sampleCheck/APITest001.java
@@ -0,0 +1,8 @@
+package com.Testingacadmy.sampleCheck;
+
+public class APITest001 {
+
+    public static void main(String[] args) {
+        System.out.println("Api testing");
+    }
+}
---
+++ src/test/java/com/Testingacadmy/sampleCheck/APITest002.java
@@ -0,0 +1,18 @@
+package com.Testingacadmy.sampleCheck;
+
+import io.restassured.RestAssured;
+
+public class APITest002 {
+    public static void main(String[] args) {
+        //Full URL: https://restful-booker.herokuapp.com/booking/1
+        RestAssured.
+                given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("booking/1")
+                .when()
+                .get()
+                .then().log().all()
+                .statusCode(200);
+
+    }
+}
